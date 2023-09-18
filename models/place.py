95a7f32575c76abc3a675d1abbc83540abcc507f
magedsaif <magedsaif21@gmail.com>
2023-09-18 18:54:02 +0200

    update place with relationship except FS

--- models/place.py
+++ models/place.py
@@ -3,10 +3,15 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
+                      )
 
 class Place(BaseModel, Base):
     """A place to stay."""
@@ -23,7 +28,13 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     amenity_ids = []
-    # for DBStorage
+
+    # review for DBStorage
     reviews = relationship('Review', cascade='all, delete, delete-orphan', backref='place')
-    # for Filestorage
+    # review for Filestorage
+    #-----------------------------------------
+
+    #amenity for DBStorage
+    amenities = relationship('Amenity', secondary=place_amenity ,viewonly=False)
+    # amenity for Filestorage
     #-----------------------------------------
