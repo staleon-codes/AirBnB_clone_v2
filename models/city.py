0f4ece9507b1d1647d78971975320a8f5a3a2933
magedsaif <magedsaif21@gmail.com>
2023-09-18 17:27:37 +0200

    Update city class

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    places = relationship('Place', cascade='all, delete, delete-orphan', backref='cities')
