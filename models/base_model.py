b62b21af0860cbb033ae928d071b364ade8ca950
Emad Anwer <emadanwer.official@gmail.com>
2023-09-20 01:29:23 +0300

    fix some bugs in base model, add cities logic

--- models/base_model.py
+++ models/base_model.py
@@ -38,8 +38,11 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        new_dict = self.__dict__.copy()
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, new_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -56,12 +59,12 @@ class BaseModel:
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
-        dictionary.update(self.__dict__)
+        dictionary.update(self.__dict__.copy())
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            dictionary.pop('_sa_instance_state')
 
         return dictionary
--- models/state.py
+++ models/state.py
@@ -3,15 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+from models import storage
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
-
-    # @property
-    # def cities(self):
-    #     """cities getter"""
-    #     return cities
+    cities = relationship('City', cascade='delete', backref='state')
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            from models.city import City
+            cities_list = []
+            for city in storage.all(City).values():
+                if class_obj.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
