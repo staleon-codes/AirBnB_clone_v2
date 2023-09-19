722776664c07fc285d262f5de32be42f094f16dd
Emad Anwer <emadanwer.official@gmail.com>
2023-09-20 01:34:19 +0300

    fix time

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,11 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
         elif 'id' not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             self.__dict__.update(kwargs)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
