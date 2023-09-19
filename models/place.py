e2a886c52b8fa9b6257e259462913ecebe80900e
Emad Anwer <emadanwer.official@gmail.com>
2023-09-20 02:06:10 +0300

    update place class

--- models/place.py
+++ models/place.py
@@ -7,16 +7,19 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), nullable=False, primary_key=True),
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), nullable=False, primary_key=True)
                       )
+
 
 class Place(BaseModel, Base):
     """A place to stay."""
 
-    __tablename__= 'places'
-    city_id = Column(String(60), ForeignKey('cities.id') , nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id') , nullable=False)
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
@@ -27,19 +30,33 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
-    # review for DBStorage
     reviews = relationship('Review', cascade='delete', backref='place')
+    amenities = relationship('Amenity', secondary=place_amenity,
+                             viewonly=False, back_populates="place_amenities")
+
     # review for Filestorage
-    #-----------------------------------------
+    # -----------------------------------------
     # @property
     # def reviews(self):
     #     """reviews getter"""
     #     return self.reviews
-    #amenity for DBStorage
-    amenities = relationship('Amenity', secondary=place_amenity ,viewonly=False, back_populates="place_amenities")
-    # amenity for Filestorage
-    #-----------------------------------------
-    # @property
-    # def amenities(self):
-    #     """amenities getter"""
-    #     return self.amenities
+    # amenity for DBStorage
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            from models.review import Review
+            reviews_list = []
+            for review in storage.all(Review).values():
+                if class_obj.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
+
+        @property
+        def amenities(self):
+            from models.amenity import Amenity
+            amenities_list = []
+            for review in storage.all(Review).values():
+                if class_obj.amenity_ids == self.id:
+                    amenities_list.append(review)
+            return amenities_list
+        # TODO amenities Setter
