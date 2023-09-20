bc5e2092ef46ca39643c0f6b3d38f01d74ef5bbc
Emad Anwer <emadanwer.official@gmail.com>
2023-09-20 13:19:25 +0300

    task 10 fix amenities

--- models/place.py
+++ models/place.py
@@ -34,7 +34,7 @@ class Place(BaseModel, Base):
 
     reviews = relationship('Review', cascade='all, delete', backref='place')
     amenities = relationship('Amenity', secondary='place_amenity',
-                             viewonly=False, back_populates="place_amenities")
+                             viewonly=False)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
