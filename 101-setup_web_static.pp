44455c5720af00c7b017a4f10b33f6c912d9f746
Emad Anwer <emadanwer.official@gmail.com>
2023-10-05 18:31:02 +0300

    t

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,37 +1,51 @@
 # Prepare your web servers
 
 file{"/data/web_static/releases/test/index.html":
-ensure => "file",
+ensure  => "file",
 content => "hello world",
-owner => "ubuntu",
-group => "ubuntu",
+owner   => "ubuntu",
+group   => "ubuntu",
 require => File["/data/web_static/releases/test"],}
 
 file{"/data/web_static/releases/test":
-ensure => "directory",
-owner => "ubuntu",
-group => "ubuntu",
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
 require => File["/data/web_static/releases"],}
 
 file{"/data/web_static":
-ensure => "directory",
-owner => "ubuntu",
-group => "ubuntu",
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
 require => File["/data"],}
 
 file{"/data/web_static/releases":
-ensure => "directory",
-owner => "ubuntu",
-group => "ubuntu",
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
 require => File["/data/web_static"],}
+
+file{"/data/web_static/shared":
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data/web_static"],}
+
+file{"/data/web_static/current":
+ensure  => "link",
+owner   => "ubuntu",
+group   => "ubuntu",
+target  => "/data/web_static/releases/test",
+require => File["/data/web_static"],}
+
 
 file{"/data":
 ensure => "directory",
-owner => "ubuntu",
-group => "ubuntu",}
+owner  => "ubuntu",
+group  => "ubuntu",}
 
 file{"/etc/nginx/sites-available/default":
-  ensure => "file",
+  ensure  => "file",
   content => "server {
     listen 80;
     listen [::]:80 default_server;
