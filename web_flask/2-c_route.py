cc300022083ced22d651837d3ecf9cf9f4b593e6
Emad Anwer <emadanwer.official@gmail.com>
2023-10-19 20:36:41 +0300

    adding escape

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """handling varible routes"""
-    return f"C {text.replace('_', ' ')}"
+    return f"C {escape(text.replace('_', ' '))}"
 
 
 if __name__ == '__main__':
