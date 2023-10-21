71e999026dafe75f56dfd25c5e17ce0622fef62c
Emad Anwer <emadanwer.official@gmail.com>
2023-10-21 14:40:10 +0300

    task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""List of states on HTML states_list"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display a HTML page: (inside the tag BODY)"""
+    sortedlist = sorted(storage.all(
+        State).values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", sorted_states_list=sortedlist)
+
+
+@app.teardown_appcontext
+def terminate(exc):
+    """close the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    """start the server"""
+    app.run(host='0.0.0.0', port=5000)
