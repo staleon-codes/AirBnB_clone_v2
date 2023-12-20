14d1185f2241b3578224d3fa2bbe97479df192f0
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-20 08:43:43 +0200

    Update

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Review module for the HBNB project."""
+"""Audit module for the HBNB project."""
 
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +7,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """Review class to store review information."""
+    """Audit class to store review information."""
 
     __tablename__='reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
