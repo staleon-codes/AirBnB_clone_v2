6ababdeacceb67964d78d85b5921403c8b7c3c8a
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-20 08:40:58 +0200

    Update

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" Citadel Module for HBNB project """
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
