3e792d0267c8194116605f549cc0ab0f820f2717
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-20 09:14:00 +0200

    Update

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" State Unitfor HBNB project """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
