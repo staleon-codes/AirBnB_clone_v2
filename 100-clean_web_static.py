23ab495d7e4375689fcf8155f955b8a8f7fb14f1
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-20 09:34:39 +0200

    Update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,7 +12,7 @@ def do_clean(number=0):
     Args:
         number (int): The number of archives to keep.
 
-    If number is 0 or 1, keeps only the most recent archive. If
+    If number is 0 or 1, keeps only recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
