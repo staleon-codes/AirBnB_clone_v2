438d36d29590db61618aaa45de1e21ebcfee51c7
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-12-20 09:15:52 +0200

    Update

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User."""
+"""The unit defines a class User."""
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes."""
+    """This class defines an operator by various attributes."""
 
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
